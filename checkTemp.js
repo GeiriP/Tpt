526a2e8b9088754afe45177f21e5184a38156933
GeiriP <101675199+GeiriP@users.noreply.github.com>
2022-10-26 15:54:44 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -3,7 +3,9 @@ function checkTemp (T){
  else if(21 <= T && T <= 40) return 1;
  else return 0;
  }
- 
- console.log(checkTemp(12)) 
- console.log(checkTemp(35))
- console.log(checkTemp(54))
+console.log(checkTemp(30));
+console.log(checkTemp(100));
+console.log(checkTemp(1));
+//katsetus
+ let T = prompt('sisesta temp');
+checkTemp();
