6cc1dd28a94e5bc5cd980566224b1b45e1a17f68
GeiriP <101675199+GeiriP@users.noreply.github.com>
2022-11-08 13:18:00 +0200

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,7 +1,7 @@
 function checkTemp (T){
  if (T <= 20) return -1;
- else if(21 <= T && T <= 40) return 1;
- else return 0;
+ else if(21 <= T && T <= 40) return 0;
+ else return 1;
  }
 console.log(checkTemp(30));
 console.log(checkTemp(100));
